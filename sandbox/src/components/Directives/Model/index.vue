34760a3cda71d767eb997210b1110bbc933bdfe0
David Christian Jones <david@dcj.codes>
2020-08-14 11:10:05 -0400

    v-model directive

---
+++ sandbox/src/components/Directives/Model/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    
+      <h3>v-model</h3>
+      <p>This directive creates a relationship between the component data and a form control.</p>
+
+      <form>
+        <input type="text" v-model="name">
+        <p>Name: {{name}}</p>
+      </form>
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Model",
+};
+</script>
