02eecb81ec7d8c198baaed61550eb7455146f4bf
David Christian Jones <david@dcj.codes>
2020-08-17 09:38:21 -0400

    v-else

--- sandbox/src/components/Directives/IfShow/index.vue
+++ sandbox/src/components/Directives/IfShow/index.vue
@@ -3,7 +3,7 @@
     <div>
       <h3>v-if</h3>
       <p>
-        This directive will mount a component if the condition is truthy, or unmount if the condition is falsey. <strong>The component is added/removed from the DOM</strong>
+        This control-flow directive will mount a component if the condition is truthy, or unmount if the condition is falsey. <strong>The component is added/removed from the DOM</strong>
       </p>
 
       <p>It is useful if you want to hide/show a component very times, ideally just the one time</p>
@@ -11,6 +11,19 @@
       <button v-on:click="toggleMounted">{{mounted ? 'Unmount' : 'Mount' }} image</button>
 
       <Img v-if="mounted" src="https://dummyimage.com/600x400/000/fff" />
+    </div>
+    <div>
+      <h3>v-else</h3>
+      <p>
+        This control-flow directive will mount a component if the condition is falsey
+      </p>
+
+      <p>It is useful if you want to render some fallback/alternate UI when a user performs some action</p>
+
+      <button v-on:click="toggleHappiness">I'm {{happy ? 'happy' : 'sad' }}</button>
+
+      <p v-if="happy">😃</p>
+      <p v-else>😡</p>
     </div>
     <div>
       <h3>v-show</h3>
@@ -35,12 +48,16 @@ export default {
   data() {
     return {
       mounted: true,
+      happy: true,
       visible: true
     };
   },
   methods: {
     toggleMounted() {
       this.mounted = !this.mounted;
+    },
+    toggleHappiness() {
+      this.happy = !this.happy;
     },
     toggleVisibility() {
       this.visible = !this.visible;
