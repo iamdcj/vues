e04cb6f2bf434689c001e61c08a821af38df4b73
David Christian Jones <david@dcj.codes>
2022-11-16 16:08:27 -0500

    add components

---
+++ components/BasicComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
+  >
+    {{ name }}
+    {{ number }}
+    <button @click="sum(5, 2)">Clickity Me</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      name: 'David Jones',
+      number: 0,
+    }
+  },
+  methods: {
+    sum(a: number, b: number) {
+      this.number = a + b
+    },
+  },
+})
+</script>
